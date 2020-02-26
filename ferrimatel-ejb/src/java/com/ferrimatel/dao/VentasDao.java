c9b05b23b1c98667c162e3614c22b013fb74aaba
Hector <Hector@Hector-PC>
2020-02-26 12:19:54 -0500

    Creacion del proyecto, creacion de clases modelo dao services controlador

--- ferrimatel-ejb/src/java/com/ferrimatel/dao/VentasDao.java
+++ ferrimatel-ejb/src/java/com/ferrimatel/dao/VentasDao.java
@@ -20,11 +20,9 @@ public class VentasDao extends Generico<Ventas> {
         try {
             List<TotalVenta> resultado;
             StringBuilder sql = new StringBuilder();
-            sql.append("SELECT ROW_NUMBER () OVER (ORDER BY PR.CODIGO_PROD) AS id, PR.CODIGO_PROD as codigoProd , PR.CANTIDAD_PROD as cantidadProd, PR.DETALLE_PROD as detalleProd"
-                    + " FROM PEDIDO P"
-                    + " INNER JOIN  VENTAS V ON P.ID_PEDIDO=V.ID_PEDIDO"
-                    + " INNER JOIN CLIENTE C ON P.ID_CLIENTE=C.ID_CLIENTE"
-                    + " INNER JOIN PRODUCTO PR ON P.ID_PROD=PR.ID_PROD");
+            sql.append("SELECT ROW_NUMBER () OVER (ORDER BY PR.CODIGO_PROD) AS id, PR.CODIGO_PROD AS codigoProd, PR.CANTIDAD_PROD as cantidadProd, PR.DETALLE_PROD as detalleProd, PR.PRECIO_PROD as puProd"
+                    + " FROM PRODUCTO PR"
+                    + " INNER JOIN PEDIDO P ON PR.ID_PROD=P.ID_PROD");
             Query query;
             query = getEntityManager().createNativeQuery(sql.toString(), TotalVenta.class);
             resultado = query.getResultList();
